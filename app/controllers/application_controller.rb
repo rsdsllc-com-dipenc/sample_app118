c39dbe67cb6003b6aa0fbd03bf98d05d2358aae8
Dipen Chauhan <150728611+rsdsllc-com-dipenc@users.noreply.github.com>
2025-01-18 11:17:05 -0600

    added welcome message from env

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  before_action :set_welcome_message
+
+  private
+  def set_welcome_message
+    @welcome_message = ENV["WELCOME_MESSAGE"]
+  end
 end
